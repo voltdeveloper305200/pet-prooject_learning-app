3c4ce3ddc19b84dbbe353d860b270a6baeab711f
voltDeveloper <vladimir305200@mail.ru>
2022-07-18 14:47:26 +0500

    завершен функционал лайков

--- src/pages/LessonDetailsPage.vue
+++ src/pages/LessonDetailsPage.vue
@@ -17,15 +17,21 @@
             <div class="details-content__info">
               <div class="author-avatar">
                 <div class="avatar-block">
-                  <img src="../assets/avatar.png" alt="" />
+                  <img :src="`${authorAvatar}`" alt="" />
                 </div>
               </div>
               <div class="lesson-info">
                 <h5>{{ title }}</h5>
                 <div class="lesson-info__bottom">
-                  <h6 class="author-name">@dianneed</h6>
+                  <h6 class="author-name">{{ authorName }}</h6>
                   <div class="lesson-feedback">
-                    <div class="lesson-feedback__likes">
+                    <div
+                      class="lesson-feedback__likes"
+                      @click="likesControl"
+                      :class="{
+                        active: activeLike,
+                      }"
+                    >
                       <svg
                         width="24"
                         height="24"
@@ -37,9 +43,15 @@
                           d="M21.3 10.08C21.019 9.74265 20.6674 9.47108 20.27 9.28447C19.8726 9.09786 19.439 9.00075 19 9H14.44L15 7.57C15.2329 6.94388 15.3105 6.2706 15.2261 5.60792C15.1416 4.94524 14.8977 4.31293 14.5152 3.76523C14.1327 3.21753 13.623 2.7708 13.0299 2.46334C12.4369 2.15589 11.778 1.99689 11.11 2C10.9176 2.0004 10.7295 2.05627 10.5681 2.16092C10.4067 2.26557 10.2789 2.41455 10.2 2.59L7.35 9H5C4.20435 9 3.44129 9.31607 2.87868 9.87868C2.31607 10.4413 2 11.2043 2 12V19C2 19.7956 2.31607 20.5587 2.87868 21.1213C3.44129 21.6839 4.20435 22 5 22H17.73C18.4318 21.9998 19.1113 21.7535 19.6503 21.304C20.1893 20.8546 20.5537 20.2303 20.68 19.54L21.95 12.54C22.0286 12.1074 22.011 11.6628 21.8987 11.2377C21.7864 10.8126 21.582 10.4174 21.3 10.08ZM7 20H5C4.73478 20 4.48043 19.8946 4.29289 19.7071C4.10536 19.5196 4 19.2652 4 19V12C4 11.7348 4.10536 11.4804 4.29289 11.2929C4.48043 11.1054 4.73478 11 5 11H7V20ZM20 12.18L18.73 19.18C18.6874 19.413 18.5635 19.6232 18.3804 19.7734C18.1973 19.9236 17.9668 20.0039 17.73 20H9V10.21L11.72 4.09C12 4.17162 12.26 4.31041 12.4837 4.49758C12.7073 4.68476 12.8897 4.91627 13.0194 5.17749C13.1491 5.43872 13.2232 5.72399 13.2371 6.0153C13.2509 6.30661 13.2043 6.59763 13.1 6.87L12.57 8.3C12.4571 8.60225 12.4189 8.92735 12.4589 9.24753C12.4988 9.56771 12.6156 9.87348 12.7993 10.1387C12.983 10.404 13.2282 10.6209 13.5139 10.7708C13.7996 10.9208 14.1173 10.9994 14.44 11H19C19.1469 10.9998 19.2921 11.0319 19.4252 11.0941C19.5582 11.1563 19.676 11.2471 19.77 11.36C19.8663 11.4713 19.9369 11.6025 19.9767 11.7443C20.0164 11.886 20.0244 12.0348 20 12.18Z"
                         />
                       </svg>
-                      <span>10</span>
+                      <span>{{ likes }}</span>
                     </div>
-                    <div class="lesson-feedback__dislikes">
+                    <div
+                      class="lesson-feedback__dislikes"
+                      @click="dislikesControl"
+                      :class="{
+                        active: activeDislike,
+                      }"
+                    >
                       <svg
                         width="24"
                         height="24"
@@ -52,7 +64,7 @@
                         />
                       </svg>
 
-                      <span>10</span>
+                      <span>{{ dislikes }}</span>
                     </div>
                     <div class="lesson-feedback__dislikes"></div>
                   </div>
@@ -61,9 +73,7 @@
             </div>
             <div class="details-content__description">
               <p>
-                In this lesson, you use navigation controllers and segues to create the navigation
-                flow of the FoodTracker app. At the end of the lesson, you’ll have a complete
-                navigation scheme and interaction flow for the app.
+                {{ description }}
               </p>
             </div>
           </div>
@@ -78,19 +88,20 @@
               class="lesson-item d-flex align-items-center"
               v-for="lesson in lessonsAll"
               :key="lesson.id"
+              @click="getLessonDetailsData"
             >
               <button class="square-btn user-btn">
                 {{ lesson.id }}
               </button>
               <div class="lesson-item__description">
-                <a>{{ lesson.title }}</a>
-                <p>{{ lesson.title }} | {{ lesson.time }} мин</p>
+                <router-link :to="`${lesson.id}`">{{ lesson.title }}</router-link>
+                <p>{{ lesson.time }} мин</p>
                 <hr />
               </div>
             </div>
           </div>
           <div class="details-btn mt-5">
-            <button :disabled="disabled" @click="get(selectedLesson.id, selectedLesson)">
+            <button :disabled="disabled" @click="makeCompleteLesson">
               <span v-if="isCompleted">&#10004; Урок пройден</span>
               <span v-else-if="waiting">Пару секунд...</span>
               <span v-else-if="!isCompleted">Завершить урок</span>
@@ -116,9 +127,16 @@ export default {
   data() {
     return {
       title: null,
+      description: null,
+      authorName: null,
+      authorAvatar: null,
       isCompleted: null,
+      likes: 0,
+      dislikes: 0,
       waiting: false,
       disabled: false,
+      activeLike: false,
+      activeDislike: false,
     };
   },
   methods: {
@@ -134,22 +152,62 @@ export default {
       if (this.$route.params.id) {
         this.selectLesson(this.$route.params.id);
         this.title = this.selectedLesson.title;
+        this.description = this.selectedLesson.description;
+        this.authorName = this.selectedLesson.authorName;
+        this.authorAvatar = this.selectedLesson.authorAvatar;
+        this.likes = this.selectedLesson.likes;
+        this.dislikes = this.selectedLesson.dislikes;
+        this.isCompleted = this.selectedLesson.isCompleted;
         if (!this.selectedLesson) {
           this.$router.back();
         }
       }
     },
-    async get(id, lesson) {
+    async makeCompleteLesson() {
       if (this.selectedLesson.isCompleted === false) {
         this.waiting = true;
         this.disabled = true;
-        await axios.put(`http://localhost:8001/lessons/${id}`, {
-          ...lesson,
+        await axios.put(`http://localhost:8001/lessons/${this.selectedLesson.id}`, {
+          ...this.selectedLesson,
           isCompleted: true,
         });
         this.waiting = false;
         this.disabled = false;
         this.selectedLesson.isCompleted = true;
+      }
+    },
+    async likesControl() {
+      this.activeLike = !this.activeLike;
+      if (this.activeLike) {
+        await axios.put(`http://localhost:8001/lessons/${this.selectedLesson.id}`, {
+          ...this.selectedLesson,
+          likes: (this.likes += 1),
+        });
+        this.selectedLesson.likes = this.likes += 1;
+      }
+      if (!this.activeLike) {
+        await axios.put(`http://localhost:8001/lessons/${this.selectedLesson.id}`, {
+          ...this.selectedLesson,
+          likes: (this.likes -= 1),
+        });
+        this.selectedLesson.likes = this.likes -= 1;
+      }
+    },
+    async dislikesControl() {
+      this.activeDislike = !this.activeDislike;
+      if (this.activeDislike) {
+        await axios.put(`http://localhost:8001/lessons/${this.selectedLesson.id}`, {
+          ...this.selectedLesson,
+          dislikes: (this.dislikes += 1),
+        });
+        this.selectedLesson.dislikes = this.dislikes += 1;
+      }
+      if (!this.activeDislike) {
+        await axios.put(`http://localhost:8001/lessons/${this.selectedLesson.id}`, {
+          ...this.selectedLesson,
+          dislikes: (this.dislikes -= 1),
+        });
+        this.selectedLesson.dislikes = this.dislikes -= 1;
       }
     },
   },
@@ -163,7 +221,6 @@ export default {
   updated() {
     this.$nextTick(function () {
       this.getLessonDetailsData();
-      this.isCompleted = this.selectedLesson.isCompleted;
     });
   },
   mounted() {
